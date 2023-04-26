b5b99141b4e6a9ab58c517b6a25443fecce9360f
Doğan Ay Şengül <20660110052@nevsehir.edu.tr>
2023-04-26 10:24:21 +0300

    beat: class to object

--- js/beat.js
+++ js/beat.js
@@ -1,27 +1,19 @@
-class Beat {
-    constructor(count) {
-        this.parent = document.querySelector(".beats");
-        this.beatCount = count;
-        this.beatRender();
-
-    }
-
+const beat = {
+    parent: document.querySelector(".beats"),
+    beatCount: 4,
     beatRender() {
-        this.parent.innerHTML = '';
-        for (let i = 0; i < this.beatCount; i++) {
-            const dot = document.createElement("div");
-            this.parent.appendChild(dot);                    
-            dot.classList.add("dot");          
-        }
-    }
-
-
+      this.parent.innerHTML = '';
+      for (let i = 0; i < this.beatCount; i++) {
+        const dot = document.createElement("div");
+        this.parent.appendChild(dot);                    
+        dot.classList.add("dot");          
+      }
+    },
     updateBeat(count) {
-        this.beatCount = count;
-        metronome.setBeatCount(count);
-        this.beatRender()
+      this.beatCount = count;
+      metronome.setBeatCount(count);
+      this.beatRender();
     }
-
-}
-
-const beat = new Beat(4)
+  };
+  
+  beat.beatRender();
